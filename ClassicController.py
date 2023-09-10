0c4354c7fd25f74572acb54f5cb7370628db2412
Alex Ghandhi <alexpghandhi@gmail.com>
2023-09-10 15:01:08 -0700

    ClassicController: initial utility function implementation

--- ClassicController.py
+++ ClassicController.py
@@ -17,11 +17,11 @@ Controller Client I2C info:
 """
 
 from machine import Pin, I2C
-from time import sleep
+from time import sleep_ms
 
 
 class ClassicController(object):
-    # Use bus address for the controller client
+    # I2C Client Address
     bus_addr = 0x52
 
     # The buffer stores the controller output
@@ -31,19 +31,24 @@ class ClassicController(object):
     # DUNDER METHODS #
     ##################
 
-    def __init__(self, sda, scl, freq) -> None:
+    def __init__(self, sda: int, scl: int, freq: int) -> None:
         """ Initialize ClassicController Object.
-
-        Decrypts the controller, sets the data to mode 3, and calibrates the controller.
 
         Args
             sda:  SDA pin
             scl:  SCL pin
             freq: frequency for I2C device
         """
-        # Decrypt Controller
-        # Set to data mode 3
+        # Create I2C client
+        self.i2c = I2C(id=0, sda=Pin(sda), scl=Pin(scl), freq=freq)
+
+        # Decrypt Controller and set Data Mode 3
+        self.decrypt_controller()
+        self.set_data_mode_3()
+
         # Calibrate Joysticks and Triggers
+        self.calibrate_joysticks()
+        self.calibrate_triggers()
 
     def __str__(self):
         # Return string representation of current button status
@@ -52,20 +57,39 @@ class ClassicController(object):
     # UTILITY FUNCTIONS #
     #####################
 
-    def decrypt_controller(self) -> bool:
+    def decrypt_controller(self) -> None:
         # Decrypt the controller
+        # write 0x55 to 0xF0 and 0x00 to 0xFB
+        self.i2c.writeto(addr=self.bus_addr, buf=b'\xF0\x55')
+        sleep_ms(10)
+        self.i2c.writeto(addr=self.bus_addr, buf=b'\xFB\x00')
+        sleep_ms(10)
 
-    def set_data_mode_3(self) -> bool:
+    def set_data_mode_3(self) -> None:
         # Set controller to data mode 3
+        # write 0x03 to 0xFE
+        self.i2c.writeto(addr=self.bus_addr, buf=b'\xFE\x03')
+        sleep_ms(10)
 
     def update(self) -> None:
         # Read data from controller and update the output buffer
+        # From wiibrew.org: Data is reported in 8 bytes at 0x08 when decrypted
+        self.i2c.writeto(addr=self.bus_addr, buf=b'\x08')
+        self.i2c.readfrom_into(addr=self.bus_addr, buf=self.buffer)
 
-    def calibrate_joysticks(self) -> bool:
+    def calibrate_joysticks(self) -> None:
         # Calibrate joysticks (store initial value as a 'center' value)
+        self.update()
+        self.joy_LX_center = self.joy_LX()
+        self.joy_LY_center = self.joy_LY()
+        self.joy_RX_center = self.joy_RX()
+        self.joy_RY_center = self.joy_RY()
 
-    def calibrate_triggers(self) -> bool:
+    def calibrate_triggers(self) -> None:
         # Calibrate analogue triggers (same method as joysticks)
+        self.update()
+        self.trigger_L_initial = self.trigger_L()
+        self.trigger_R_initial = self.trigger_R()
 
     ##################
     # BUTTON GETTERS #
@@ -112,12 +136,24 @@ class ClassicController(object):
 
     def trigger_L(self):
         # Get value of Left trigger
+        return int(self.buffer[4])
 
     def trigger_R(self):
         # Get value of Right trigger
+        return int(self.buffer[5])
 
-    def joy_L(self):
-        # Get value of Left joystick
+    def joy_LX(self):
+        # Get value of Left joystick x-axis
+        return int(self.buffer[0])
 
-    def joy_R(self):
-        # Get value of Right joystick
+    def joy_LY(self):
+        # Get value of Left joystick y-axis
+        return int(self.buffer[2])
+
+    def joy_RX(self):
+        # Get value of Right joystick x-axis
+        return int(self.buffer[1])
+
+    def joy_RY(self):
+        # Get value of Right joystick y-axis
+        return int(self.buffer[3])
