3e623389665d6e533f353ef690d8ef9d766a6ea5
Alex Ghandhi <alexpghandhi@gmail.com>
2023-09-10 22:37:11 -0700

    ClassicController: Working version
    Bytes are read properly and Joysticks and Triggers are read correctly.
    
    More testing is needed for digital button presses.

--- ClassicController.py
+++ ClassicController.py
@@ -24,9 +24,6 @@ class ClassicController(object):
     # I2C Client Address
     bus_addr: int = 0x52
 
-    # The buffer stores the controller output
-    buffer = bytearray(8)
-
     ##################
     # DUNDER METHODS #
     ##################
@@ -40,55 +37,74 @@ class ClassicController(object):
             scl:  SCL pin
             freq: frequency for I2C device
         """
+        # The buffer stores the controller output
+        self.buffer = bytearray(8)
+
         # Create I2C client
         self.i2c = I2C(id=id, sda=Pin(sda), scl=Pin(scl), freq=freq)
         sleep_ms(10)
-        print("Performing bus scan")
-        print(self.i2c.scan())
-        print("Bus scan complete")
+
+        # Run a bus scan, this should show '[82]' (0x52)
+        print(f"Bus Scan Results: {self.i2c.scan()}")
+        sleep_ms(10)
 
         # Decrypt Controller and set Data Mode 3
         print("Decrypting Controller")
         self.decrypt_controller()
+
+        # Check extension ID
+        extensionID = bytearray(6)
+        self.i2c.writeto(self.bus_addr, b'\xFA')
+        sleep_ms(10)
+        self.i2c.readfrom_into(self.bus_addr, extensionID)
+        sleep_ms(10)
+        print(f"Extension ID: {extensionID}")
+
         print("Setting Data Mode 3")
         self.set_data_mode_3()
 
         # Calibrate Joysticks and Triggers
-        print("Calibrating")
+        print("Calibrating...", end="")
         self.calibrate_joysticks()
         self.calibrate_triggers()
-        print("Calibration Complete")
+        print("Complete!")
 
     def __str__(self):
+        #print(f"Raw Output: {self.buffer}")
         self.update()
-        print(f"Left Joy X: {self.joy_LX()}")
+        print(f"Left Joy: {self.joy_LX()}, {self.joy_LY()}")
+        print(f"Right Joy: {self.joy_RX()}, {self.joy_RY()}")
+        print(f"Triggers: L {self.trigger_L()}, R {self.trigger_R()}")
 
     #####################
     # UTILITY FUNCTIONS #
     #####################
 
     def decrypt_controller(self) -> None:
-        print("writing 0x55 to 0xF0")
+        print("writing 0x55 to 0x(4)A400F0")
+        #self.i2c.writeto_mem(self.bus_addr, 0x4A400F0, b'\x55')
         self.i2c.writeto(self.bus_addr, b'\xF0\x55')
-        sleep_ms(50)
-        print("writing 0x00 to 0xFB")
+        sleep_ms(10)
+
+        print("writing 0x00 to 0x(4)A400FB")
+        #self.i2c.writeto_mem(self.bus_addr, 0x4A400FB, b'\x00')
         self.i2c.writeto(self.bus_addr, b'\xFB\x00')
-        sleep_ms(50)
+        sleep_ms(10)
 
     def set_data_mode_3(self) -> None:
-        print("writing 0x03 to 0xFE")
+        print("writing 0x03 to 0x(4)A400FE")
+        #self.i2c.writeto_mem(self.bus_addr, 0x4A400FE, b'\x03')
         self.i2c.writeto(self.bus_addr, b'\xFE\x03')
-        sleep_ms(50)
+        sleep_ms(10)
 
     def update(self) -> None:
         # Read data from controller and update the output buffer
         # From wiibrew.org: Data is reported in 8 bytes at 0x08 when decrypted
-        print("Requesting data from bus")
-        self.i2c.writeto(self.bus_addr, b'\x08')
-        print("Reading button data")
+        #self.i2c.readfrom_mem_into(self.bus_addr, 0xA40008, self.buffer)
+        self.i2c.writeto(self.bus_addr, b'\x00\x00')
+        sleep_ms(5)
         self.i2c.readfrom_into(self.bus_addr, self.buffer)
-        print("Done updating")
-        sleep_ms(50)
+        sleep_ms(5)
 
     def calibrate_joysticks(self) -> None:
         # Find center values for joysticks
@@ -188,7 +204,7 @@ class ClassicController(object):
 # Debug: create instance and print controller output
 if __name__ == '__main__':
     print("Creating Controller Object")
-    controller = ClassicController(id=0, sda=8, scl=9, freq=400000)
+    controller = ClassicController(id=0, sda=8, scl=9, freq=100000)
     while (True):
         print(controller)
         sleep_ms(100)
